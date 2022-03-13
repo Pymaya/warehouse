6dac683c9610263d259c2faabf82b53572067f53
marc <49933584+Pymaya@users.noreply.github.com>
2022-03-13 20:53:37 +0800

    Add files via upload

---
+++ buschargesys.py
@@ -0,0 +1,32 @@
+# everything is idealized.
+allstops={
+    'a':0,
+    'b':1,
+    'c':2,
+    'd':3,
+    'e':4,
+    'f':5,
+    'g':6,
+    'h':7,
+    'i':8,
+    'j':9,
+    'k':10,
+    'l':11,
+    'm':12,
+} # container for all bus stops.
+
+
+def stops_interval(startstop,endstop):
+    v=allstops[startstop]-allstops[endstop]
+    if v>0:
+        c=(13-abs(v))*1.8
+    else:
+        c=abs(v)*1.8
+    return c
+'''     I assume its routine is a loop. If endstop is somewhere before startstop on the routine,
+        passenger might go to the bottom stop and take a new one starts from the top stop.
+'''
+print("Welcome to take bus.")
+start=input("Your start stop is:")
+end=input("Your end stop is:")
+print("The charge is:"+str(stops_interval(start,end))+".")
