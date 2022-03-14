6f92541d646da6bb7be84a2c1bd8867b45831a3c
marc <49933584+Pymaya@users.noreply.github.com>
2022-03-14 13:15:07 +0800

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,3 +1,4 @@
+‘’‘
 from mergedict import mergedict
 test_a={
     'name':'jack',
@@ -12,3 +13,24 @@ vv={}
 vv=mergedict(f_1,f_2)
 
 print(vv)
+’‘’
+
+magicians=[
+    'jack',
+    'tom',
+    'peter',
+    'gwen',
+]
+def show_magician(magician):
+    for magician in magicians:
+        print(magician.title())
+
+show_magician(magicians)
+
+def make_great(magician):
+    great_magicians=[]
+    for magician in magicians:
+        great_magicians.append(magician.title()+' the Great')
+    return great_magicians
+
+print(make_great(magicians))
